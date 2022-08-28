a4b716f88f8e0e97c4855a4cf984ee6dd2f52c24
Immanuel Vivek <immanuelvivek40609@gmail.com>
2022-08-28 21:46:16 +0530

    Made some necessary changes

--- client/src/Sudoku-Functional-Components/Board.js
+++ client/src/Sudoku-Functional-Components/Board.js
@@ -4,13 +4,28 @@ import Cell from "./Cell";
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.board = new Array(81);
-    for (let i = 0; i < 81; i++) {
-      this.board[i] = new Cell(0);
+    this.board = new Array(9);
+    for (let i = 0; i < 9; i++) {
+      this.board[i] = new Array(9);
+    }
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        this.board[i][j] = new Cell(0);
+      }
     }
   }
+
   render() {
-    return <div>{this.board[1].show()}</div>;
+    const flatArray = this.board.flat();
+
+    return (
+      <div>
+        {flatArray.map((cell, index) => (
+          <div>{cell.show()}</div>
+        ))}
+      </div>
+    );
   }
 }
 
